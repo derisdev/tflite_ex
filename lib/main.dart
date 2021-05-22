b0a8cc5fa74b8c6378f93df319026b6d9d073758
Erderis <derisoffice@gmail.com>
2021-05-22 11:59:25 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:tensorflow/scan_image.dart';
 
-import 'homepage.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
 
-void main() => runApp(MyApp());
+  runApp(MyApp(
+    camera: firstCamera,
+  ));
+}
 
 class MyApp extends StatelessWidget {
+  final CameraDescription camera;
+
+  const MyApp({Key key, this.camera}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Tensorflow Lite',
-      home: HomePage(),
-    );
+        title: 'TF Lite',
+        home: ScanImage(
+          camera: camera,
+        ));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,78 +1,77 @@
-import 'dart:io';
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:scalable_image/scalable_image.dart';
 
-import 'package:flutter/material.dart';
-import 'package:scalable_image/scalable_image.dart';
+// class Result extends StatefulWidget {
+//   final List listOutput;
+//   final File image;
+//   Result({this.listOutput, this.image});
+//   @override
+//   _ResultState createState() => _ResultState();
+// }
 
-class Result extends StatefulWidget {
-  final List listOutput;
-  final File image;
-  Result({this.listOutput, this.image});
-  @override
-  _ResultState createState() => _ResultState();
-}
-
-class _ResultState extends State<Result> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          ScalableImage(
-              imageProvider: new FileImage(widget.image),
-              dragSpeed: 4.0,
-              maxScale: 16.0),
-          DraggableScrollableSheet(
-              maxChildSize: 0.96,
-              minChildSize: 0.05,
-              initialChildSize: 0.15,
-              builder:
-                  (BuildContext context, ScrollController scrollController) {
-                return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(30.0),
-                        topRight: const Radius.circular(30.0),
-                      ),
-                    ),
-                    child: SingleChildScrollView(
-                      controller: scrollController,
-                      child: Column(
-                        children: [
-                          SizedBox(height: 30),
-                          Container(
-                            width: 50,
-                            height: 7,
-                            decoration: BoxDecoration(
-                                color: Color(0xffCBD3DC),
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          ListView.builder(
-                              physics: NeverScrollableScrollPhysics(),
-                              shrinkWrap: true,
-                              itemCount: widget.listOutput.length,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20.0, vertical: 5),
-                                  child: Center(
-                                      child: Text(
-                                    widget.listOutput[index]['label'],
-                                    style: TextStyle(
-                                        fontSize: 17,
-                                        fontWeight: FontWeight.bold),
-                                  )),
-                                );
-                              }),
-                          SizedBox(
-                            height: 30,
-                          )
-                        ],
-                      ),
-                    ));
-              })
-        ],
-      ),
-    );
-  }
-}
+// class _ResultState extends State<Result> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           ScalableImage(
+//               imageProvider: new FileImage(widget.image),
+//               dragSpeed: 4.0,
+//               maxScale: 16.0),
+//           DraggableScrollableSheet(
+//               maxChildSize: 0.96,
+//               minChildSize: 0.05,
+//               initialChildSize: 0.15,
+//               builder:
+//                   (BuildContext context, ScrollController scrollController) {
+//                 return Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.white,
+//                       borderRadius: BorderRadius.only(
+//                         topLeft: const Radius.circular(30.0),
+//                         topRight: const Radius.circular(30.0),
+//                       ),
+//                     ),
+//                     child: SingleChildScrollView(
+//                       controller: scrollController,
+//                       child: Column(
+//                         children: [
+//                           SizedBox(height: 30),
+//                           Container(
+//                             width: 50,
+//                             height: 7,
+//                             decoration: BoxDecoration(
+//                                 color: Color(0xffCBD3DC),
+//                                 borderRadius: BorderRadius.circular(20)),
+//                           ),
+//                           ListView.builder(
+//                               physics: NeverScrollableScrollPhysics(),
+//                               shrinkWrap: true,
+//                               itemCount: widget.listOutput.length,
+//                               itemBuilder: (context, index) {
+//                                 return Padding(
+//                                   padding: const EdgeInsets.symmetric(
+//                                       horizontal: 20.0, vertical: 5),
+//                                   child: Center(
+//                                       child: Text(
+//                                     widget.listOutput[index]['label'],
+//                                     style: TextStyle(
+//                                         fontSize: 17,
+//                                         fontWeight: FontWeight.bold),
+//                                   )),
+//                                 );
+//                               }),
+//                           SizedBox(
+//                             height: 30,
+//                           )
+//                         ],
+//                       ),
+//                     ));
+//               })
+//         ],
+//       ),
+//     );
+//   }
+// }
